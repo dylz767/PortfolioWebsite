55b5f8f98ddbb8203d2d2a3f4e412d261753a64b
dylz767 <d.angol@live.co.uk>
2024-11-08 22:24:15 +0000

    changes

--- img/DND-Dylz (@dnd_dylz) • Instagram photos and videos_files/1hQ3zujdN7b.js
+++
@@ -1,3 +0,0 @@
-;/*FB_PKG_DELIM*/
-
-__d("useMAWUnsupportedBrowserNUX",["MAWSetupUtils","WebStorage","gkx"],(function(a,b,c,d,e,f,g){"use strict";var h;function a(){var a=d("MAWSetupUtils").useIsIDBOperational();a=c("gkx")("24101")===!0||c("gkx")("1778")||!a||(h||(h=c("WebStorage"))).getLocalStorage()==null;var b=a&&!c("gkx")("1778");return{shouldHideEBFlow:b,shouldShowQP:a}}g.useMAWUnsupportedBrowserNUX=a}),98);
--- img/photo233_files/Y5dlTGPMUW2.js
+++
@@ -1,3 +0,0 @@
-;/*FB_PKG_DELIM*/
-
-__d("LSIssueThreadCapabilitySyncTask",[],(function(a,b,c,d,e,f){function a(){var a=arguments,b=a[a.length-1],c=[];return b.resolve(c)}e.exports=a}),null);
